e3aa58c65372f47522573df9e0d70fab88a9c66c
Wix Remote Machine <support@wix.com>
2026-03-11 12:44:25 +0000

    Add button to Barraco page

--- src/components/pages/CasaPage.tsx
+++ src/components/pages/CasaPage.tsx
@@ -1,7 +1,12 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { Button } from '@/components/ui/button';
 
 export default function CasaPage() {
+  const handleButtonClick = () => {
+    alert('Botão clicado!');
+  };
+
   return (
     <div
       className="min-h-screen w-full flex flex-col"
@@ -13,6 +18,11 @@ export default function CasaPage() {
       }}
     >
       <Header />
+      <div className="w-full flex justify-center pt-8">
+        <Button onClick={handleButtonClick} className="px-8 py-3 text-lg">
+          Clique aqui
+        </Button>
+      </div>
       <main className="flex-1 flex items-center justify-center">
         {/* Content area - you can add content here */}
       </main>
