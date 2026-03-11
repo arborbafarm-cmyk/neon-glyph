ca53dafccbe92cf47e92c5dbc9dc481f0c86cd0c
Wix Remote Machine <support@wix.com>
2026-03-11 04:33:43 +0000

    Tornar cofre visível no cabeçalho

--- src/store/dirtyMoneyStore.ts
+++ src/store/dirtyMoneyStore.ts
@@ -4,10 +4,12 @@ interface DirtyMoneyStore {
   dirtyMoney: number;
   addDirtyMoney: (amount: number) => void;
   resetDirtyMoney: () => void;
+  setDirtyMoney: (amount: number) => void;
 }
 
 export const useDirtyMoneyStore = create<DirtyMoneyStore>((set) => ({
   dirtyMoney: 0,
   addDirtyMoney: (amount: number) => set((state) => ({ dirtyMoney: state.dirtyMoney + amount })),
   resetDirtyMoney: () => set({ dirtyMoney: 0 }),
+  setDirtyMoney: (amount: number) => set({ dirtyMoney: amount }),
 }));
