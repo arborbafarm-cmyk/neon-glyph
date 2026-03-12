400dff782af8a568ce4f4c97c99f35c4b9c4c8ec
Wix Remote Machine <support@wix.com>
2026-03-12 17:21:49 +0000

    Fix blank page display issue

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -8,6 +8,18 @@ export const Head = () => {
       {/* Font preloads */}
       <link rel="preload" href="/fonts/leaguespartan/v15/kJEqBuEW6A0lliaV_m88ja5TwvZwLZmXD4Zh.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
       <link rel="preload" href="/fonts/spacegrotesk/v22/V8mDoQDjQSkFtoMM3T6r8E7mPbF4C_k3HqU.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+      {/* Ensure page is visible */}
+      <style>{`
+        html, body, #root {
+          width: 100%;
+          height: 100%;
+          margin: 0;
+          padding: 0;
+          display: block;
+          visibility: visible;
+          opacity: 1;
+        }
+      `}</style>
     </>
   );
 };
