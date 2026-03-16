58c58233fccf2387fc33fb0a625d04fdd7a1051e
Wix Remote Machine <support@wix.com>
2026-03-16 03:13:38 +0000

    Trocar imagem de game2 ao atingir nível 40

--- src/components/pages/Game2Page.tsx
+++ src/components/pages/Game2Page.tsx
@@ -94,6 +94,9 @@ export default function Game2Page() {
 
   // Determine which image to display based on player level
   const getDisplayImage = (): string => {
+    if (playerLevel >= 40) {
+      return 'https://static.wixstatic.com/media/50f4bf_9d7eef85347c4c5d8ea84266bb417092~mv2.png';
+    }
     if (playerLevel >= 30) {
       return 'https://static.wixstatic.com/media/50f4bf_575530527bf046f69c61086e07a9c7e1~mv2.png';
     }
