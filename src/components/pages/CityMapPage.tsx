4f3e02182c9ee23118bd3eb712a79a52327c27ae
Wix Remote Machine <support@wix.com>
2026-03-18 04:52:50 +0000

    Apagando conteúdo da página

--- src/components/pages/CityMapPage.tsx
+++ src/components/pages/CityMapPage.tsx
@@ -1,9 +1,6 @@
-import Multiplayer3DMap from '@/components/game/Multiplayer3DMap';
-
 export default function CityMapPage() {
   return (
-    <div className="w-screen h-screen bg-black">
-      <Multiplayer3DMap />
+    <div className="w-full h-screen bg-black">
     </div>
   );
 }
