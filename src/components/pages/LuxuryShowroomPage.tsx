fcd811e831d7d24128d944937aa50ad2dcec49d4
Wix Remote Machine <support@wix.com>
2026-03-19 06:18:36 +0000

    Remover imagens e adicionar background

--- src/components/pages/LuxuryShowroomPage.tsx
+++ src/components/pages/LuxuryShowroomPage.tsx
@@ -1,18 +1,21 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import CharacterDialog from '@/components/CharacterDialog';
-import LuxuryShop from '@/components/LuxuryShop';
-import { motion } from 'framer-motion';
 import { Image } from '@/components/ui/image';
-import { useGameStore } from '@/store/gameStore';
 
 export default function LuxuryShowroomPage() {
-  const { playerLevel } = useGameStore();
-
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <LuxuryShop />
+      
+      {/* Background Section */}
+      <div className="flex-1 w-full">
+        <Image
+          src="https://static.wixstatic.com/media/50f4bf_8787d9f97cfa4afe99b4bdd843fde7da~mv2.png"
+          alt="Luxury Showroom Background"
+          className="w-full h-full object-cover"
+          width={1600}
+        />
+      </div>
 
       <Footer />
     </div>
