17289f56427c1b041cd7c4e33811854643daceaf
Wix Remote Machine <support@wix.com>
2026-03-19 06:20:15 +0000

    Ajustar background para 90%

--- src/components/pages/LuxuryShowroomPage.tsx
+++ src/components/pages/LuxuryShowroomPage.tsx
@@ -8,13 +8,15 @@ export default function LuxuryShowroomPage() {
       <Header />
       
       {/* Background Section */}
-      <div className="flex-1 w-full">
-        <Image
-          src="https://static.wixstatic.com/media/50f4bf_8787d9f97cfa4afe99b4bdd843fde7da~mv2.png"
-          alt="Luxury Showroom Background"
-          className="w-full h-full object-cover"
-          width={1600}
-        />
+      <div className="flex-1 w-full flex items-center justify-center p-4">
+        <div className="w-full h-[90vh] max-w-[90%]">
+          <Image
+            src="https://static.wixstatic.com/media/50f4bf_8787d9f97cfa4afe99b4bdd843fde7da~mv2.png"
+            alt="Luxury Showroom Background"
+            className="w-full h-full object-contain"
+            width={1600}
+          />
+        </div>
       </div>
 
       <Footer />
