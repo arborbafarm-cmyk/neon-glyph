30ee130cbd2f497a25207f8aca76a595c8f0065b
Wix Remote Machine <support@wix.com>
2026-03-20 22:26:51 +0000

    Manual code changes

--- src/components/pages/CameraControlPage.tsx
+++
@@ -1,9 +0,0 @@
-import CameraControlScene from '@/components/CameraControlScene';
-
-export default function CameraControlPage() {
-  return (
-    <div className="w-full h-screen bg-background">
-      <CameraControlScene />
-    </div>
-  );
-}
