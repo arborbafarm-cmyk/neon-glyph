525d8ba0538bef75d266c8fd8abe1a688ce2fb2c
Wix Remote Machine <support@wix.com>
2026-03-25 23:04:16 +0000

    Corrigir arquivos de store

--- src/store/cleanMoneyStore.ts
+++ src/store/cleanMoneyStore.ts
@@ -1,14 +1,6 @@
 import { create } from 'zustand';
 
-interface CleanMoneyStore {
-  cleanMoney: number;
-  addCleanMoney: (amount: number) => void;
-  removeCleanMoney: (amount: number) => void;
-  resetCleanMoney: () => void;
-  setCleanMoney: (amount: number) => void;
-}
-
-export const useCleanMoneyStore = create<CleanMoneyStore>((set) => ({
+export const useCleanMoneyStore = create((set) => ({
   cleanMoney: 1000000000,
   addCleanMoney: (amount: number) => set((state) => ({ cleanMoney: state.cleanMoney + amount })),
   removeCleanMoney: (amount: number) => set((state) => ({ cleanMoney: Math.max(0, state.cleanMoney - amount) })),
--- src/store/dirtyMoneyStore.ts
+++ src/store/dirtyMoneyStore.ts
@@ -1,15 +1,7 @@
 import { create } from 'zustand';
 
-interface DirtyMoneyStore {
-  dirtyMoney: number;
-  addDirtyMoney: (amount: number) => void;
-  removeDirtyMoney: (amount: number) => void;
-  resetDirtyMoney: () => void;
-  setDirtyMoney: (amount: number) => void;
-}
-
-export const useDirtyMoneyStore = create<DirtyMoneyStore>((set) => ({
-  dirtyMoney: 1000000000,
+export const useDirtyMoneyStore = create((set) => ({
+  dirtyMoney: 0,
   addDirtyMoney: (amount: number) => set((state) => ({ dirtyMoney: state.dirtyMoney + amount })),
   removeDirtyMoney: (amount: number) => set((state) => ({ dirtyMoney: Math.max(0, state.dirtyMoney - amount) })),
   resetDirtyMoney: () => set({ dirtyMoney: 0 }),
